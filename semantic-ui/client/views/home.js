295ddc92aee358beb38919109ba91af5c7f1d453
zhjch05 <zjc1996@brandeis.edu>
2015-06-12 13:40:30 -0400

    legal moves

--- semantic-ui/client/views/home.js
+++
@@ -1,8 +0,0 @@
-Template.home.rendered = function(){
-	var cfg = {
-	  draggable: true,
-	  dropOffBoard: 'snapback', // this is the default
-	  position: 'start'
-	};
-	var board = new ChessBoard('board',cfg);
-}
